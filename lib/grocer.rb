c369a5229068b4ca13cadfa71b050b14469a542c
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-08-02 21:38:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,5 +1,5 @@
 def find_item_by_name_in_collection(name, collection)
-  grocery_shelf. # Implement me first!
+  grocery_shelf.map do | # Implement me first!
   #
   # Consult README for inputs and outputs
 
